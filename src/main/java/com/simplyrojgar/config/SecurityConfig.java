787a4b6f0d27788df891118a4e00f0297ac48e2a
Shashank Mungantiwar <srmungantiwar1@gmail.com>
2023-07-29 01:56:28 +0530

    Added initial code for Login and Registration

--- src/main/java/com/simplyrojgar/config/SecurityConfig.java
+++ src/main/java/com/simplyrojgar/config/SecurityConfig.java
@@ -2,25 +2,54 @@ package com.simplyrojgar.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
 	
+	private UserDetailsService userDetailsService;
+
+    public SecurityConfig(UserDetailsService userDetailsService){
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public static PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+	
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .build();
+    }
+    
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//      http.csrf().disable()
-//                .authorizeHttpRequests((authorize) ->
-//                        authorize.requestMatchers(HttpMethod.GET, "/languages/**").permitAll()
-//                                .requestMatchers(HttpMethod.DELETE,"/languages/id/**").permitAll()
-//                                .requestMatchers(HttpMethod.POST,"/languages/**").permitAll()
-//                                .anyRequest().authenticated()
-//                );
-        http.authorizeRequests().anyRequest().permitAll();
-
+      http.csrf().disable()
+                .authorizeHttpRequests((authorize) ->
+                        authorize.requestMatchers(HttpMethod.GET, "/languages/**").permitAll()
+                                .requestMatchers(HttpMethod.DELETE,"/languages/id/**").permitAll()
+                                .requestMatchers(HttpMethod.POST,"/languages/**").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/api/**").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/swagger-ui/**").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/api-docs/**").permitAll()
+                                .requestMatchers("/api/auth/**").permitAll()
+                                .anyRequest().authenticated()
+                );
+      	/*
+      	 * To allow all URLs use below code
+      	 * http.authorizeRequests().anyRequest().permitAll();
+      	 */
         return http.build();
     }
 }

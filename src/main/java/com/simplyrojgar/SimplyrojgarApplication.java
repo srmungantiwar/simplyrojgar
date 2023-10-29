7e0ffb0c961177e6ca6801964b897c77e035ee5b
Shashank Mungantiwar <srmungantiwar1@gmail.com>
2023-10-29 16:46:15 +0530

    added changes for upload to server

--- src/main/java/com/simplyrojgar/SimplyrojgarApplication.java
+++ src/main/java/com/simplyrojgar/SimplyrojgarApplication.java
@@ -2,9 +2,10 @@ package com.simplyrojgar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SimplyrojgarApplication {
+public class SimplyrojgarApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SimplyrojgarApplication.class, args);
